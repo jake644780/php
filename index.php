6114e1f6c2b6c62754fc770caff41c270a1fe421
jake644780 <yesn7722@gmail.com>
2024-03-11 18:29:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>3MinusPerfumes</title>
+</head>
+<body>
+    username: <input type="text" name="username"><br>
+    email: <input type="text" name="email"><br>
+    password: <input type="text" name="password"><br>
+    <input type="submit" name="submit" value="register"> or <a href="login.php">login</a>
+</body>
+</html>
