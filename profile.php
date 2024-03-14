bd0982f188cc42c7c723c11075e15fd6c6e2e560
jake644780 <yesn7722@gmail.com>
2024-03-14 22:04:53 +0100

    updated DB, added more echo-s to profile.php

--- profile.php
+++ profile.php
@@ -16,16 +16,26 @@
     </head>
     <body>
     <?php include("header.php"); 
+    echo "<center>";
     if (@$_GET['id']){
         $q = "select * from users where id='".$_GET['id']."'";
         $result = $conn->query($q);
         $rows = mysqli_num_rows($result);
         if (mysqli_num_rows($result) != 0){
-            echo "success";
+            while ($row = mysqli_fetch_assoc($result)){
+                echo "<h1>".$row['username']."</h1><br>";
+                echo "date registered: ".$row['date']."<br>";
+                echo "email: ".$row['email']."<br>";
+                echo "replies: ".$row['replies']."<br>";
+                echo "score: ".$row['score']."<br>";
+                echo "topics created: ".$row['topics']."<br>";
+
+            }
         } else{
             echo "couldn't find id";
         }
     }
+    echo "</center>";
     ?>
     </body>
 </html>
