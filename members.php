633080e51c5d06f78ad20f6c0e760ab810167c2d
jake644780 <yesn7722@gmail.com>
2024-03-15 17:23:34 +0100

    header center alignment

--- members.php
+++ members.php
@@ -2,7 +2,6 @@
     session_start();
     require('connect.php');
     if (@$_SESSION["username"]){
-        echo "welcome ".$_SESSION["username"];
     } else{
         echo 'you must be logged in....<br><a href="login.php">login here</a> or <a href="register.php">register</a>';
     }
