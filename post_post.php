8f933d56e9ce95fc5b01791854311ef84200d7b6
jake644780 <yesn7722@gmail.com>
2024-03-19 12:52:31 +0100

    going back to topics page

--- post_post.php
+++ post_post.php
@@ -56,6 +56,8 @@ if (isset($_POST['submit'])){
                 $result = $conn->query("INSERT INTO posts (`post_id`,`post_name`,`post_content`,`post_creator`,`date`,`master_id`) VALUES ('','".$post_name."','".$content."', '".$_SESSION['username']."', '".$date."', '".$master_id."')");
                 if ($result){
                     echo "success";
+                    header("Location: topic.php?id=".$master_id);
+
                 }
             }
         } else echo "name has to be atleast 6 chars long and less than 70";
